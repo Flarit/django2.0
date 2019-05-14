137dac5f8d1c4016a49113654e3bb39de9dfbc6a
Flarit <j1313@yandex.ru>
2019-05-14 13:58:43 +0300

    DZ2

---
+++ DZ-2/geekshop/adminapp/views.py
@@ -0,0 +1,196 @@
+from authapp.models import ShopUser
+from django.shortcuts import get_object_or_404, render
+from mainapp.models import Product, ProductCategory
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse_lazy, reverse
+from authapp.forms import ShopUserRegisterForm
+from adminapp.forms import ShopUserAdminEditForm, ProductCategoryEditForm
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.decorators import user_passes_test
+
+
+class IsSuperUserView(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.is_superuser
+
+
+class ShopUserListView(IsSuperUserView, ListView):
+    model = ShopUser
+    queryset = ShopUser.objects.all().order_by('-is_active', '-is_superuser', '-is_staff', 'username')
+    template_name = 'adminapp/users.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ShopUserListView, self).get_context_data(**kwargs)
+        context['title'] = 'Список пользователей'
+        return context
+
+
+class ShopUserCreateView(CreateView):
+    model = ShopUser
+    template_name = 'adminapp/user_update.html'
+    fields = '__all__'
+    success_url = reverse_lazy('admin:users')
+
+    def get_context_data(self, **kwargs):
+        context = super(ShopUserCreateView, self).get_context_data(**kwargs)
+        context['title'] = 'Создание пользователя'
+        return context
+
+
+class ShopUserUpdateView(UpdateView):
+    model = ShopUser
+    template_name = 'adminapp/user_update.html'
+    fields = '__all__'
+    success_url = reverse_lazy('admin:users')
+
+    def get_context_data(self, **kwargs):
+        context = super(ShopUserUpdateView, self).get_context_data(**kwargs)
+        context['title'] = 'Изменение пользователя'
+        return context
+
+
+class ShopUserDeleteView(DeleteView):
+    model = ShopUser
+    template_name = 'adminapp/user_delete.html'
+    success_url = reverse_lazy('admin:users')
+
+    def get_context_data(self, **kwargs):
+        context = super(ShopUserDeleteView, self).get_context_data(**kwargs)
+        context['title'] = 'удаление пользователя'
+        return context
+
+    def delete(self, request, *args, **kwargs):
+        user = get_object_or_404(ShopUser, pk=kwargs['pk'])
+        user.is_active = False
+        user.save()
+        return HttpResponseRedirect(self.success_url)
+
+
+class ProductCategoryListView(IsSuperUserView, ListView):
+    model = ProductCategory
+    template_name = 'adminapp/categories.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductCategoryListView, self).get_context_data(**kwargs)
+        context['title'] = 'Список категорий'
+        return context
+
+
+class ProductCategoryCreateView(CreateView):
+    model = ProductCategory
+    template_name = 'adminapp/categories_update.html'
+    fields = '__all__'
+    success_url = reverse_lazy('admin:categories')
+
+
+class ProductCategoryUpdateView(UpdateView):
+    model = ProductCategory
+    template_name = 'adminapp/categories_update.html'
+    fields = '__all__'
+    success_url = reverse_lazy('admin:categories')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Изменение категории'
+        return context
+
+
+class ProductCategoryDeleteView(DeleteView):
+    model = ProductCategory
+    template_name = 'adminapp/categories_delete.html'
+    success_url = reverse_lazy('admin:categories')
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.is_active = False
+        self.object.save()
+
+        return HttpResponseRedirect(self.get_success_url())
+
+
+class ProductView(ListView):
+    model = Product
+    template_name = 'adminapp/products.html'
+
+    def get_queryset(self):
+        return Product.objects.filter(category__pk=self.kwargs['pk']).order_by('name')
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductView, self).get_context_data(**kwargs)
+        context['title'] = 'Список пользователей'
+        context['category'] = get_object_or_404(ProductCategory, pk=self.kwargs['pk'])
+        return context
+# def products(request, pk):
+#     title = 'админка/продукт'
+#
+#     category = get_object_or_404(ProductCategory, pk=pk)
+#     products_list = Product.objects.filter(category__pk=pk).order_by('name')
+#
+#     content = {
+#         'title': title,
+#         'category': category,
+#         'objects': products_list,
+#     }
+#
+#     return render(request, 'adminapp/products.html', content)
+
+
+class ProductListView(IsSuperUserView, ListView):
+    model = Product
+    template_name = 'adminapp/product_read.html'
+
+    def get_queryset(self):
+        return Product.objects.filter(pk=self.kwargs['pk'])
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductListView, self).get_context_data(**kwargs)
+        print(context)
+        context['title'] = 'Список продуктов'
+        print(context)
+        return context
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    template_name = 'adminapp/product_update.html'
+    fields = '__all__'
+    success_url = reverse_lazy('admin:categories')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = 'adminapp/product_update.html'
+    fields = '__all__'
+    success_url = reverse_lazy('admin:categories')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Изменение товара'
+        return context
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'adminapp/product_delete.html'
+    success_url = reverse_lazy('admin:categories')
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDeleteView, self).get_context_data(**kwargs)
+        context['title'] = 'удаление товара'
+        return context
+
+    def delete(self, request, *args, **kwargs):
+        prod = get_object_or_404(Product, pk=kwargs['pk'])
+        prod.is_active = False
+        prod.save()
+        return HttpResponseRedirect(self.success_url)
+
+
+
+
+
+
+
+
---
+++ DZ-2/geekshop/mainapp/views.py
@@ -0,0 +1,85 @@
+from django.shortcuts import render
+from .models import Product, ProductCategory
+from django.shortcuts import get_object_or_404
+from basketapp.models import Basket
+import random
+
+
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    else:
+        return []
+
+
+def get_hot_product():
+    products = Product.objects.filter(is_active=True)
+    return random.sample(list(products), 1)[0]
+
+
+def get_same_products(hot_product):
+    same_products = Product.objects.filter(category=hot_product.category). \
+                        exclude(pk=hot_product.pk)[:3]
+
+    return same_products
+
+
+def main(request):
+    prod_cat = ProductCategory.objects.all()
+    return render(request, 'mainapp/index.html', context={'user': request.user, 'menu': {'main': 'Главная', 'catalogue': 'Каталог', 'contacts': 'Контакты'}, 'page': 'Главная', 'prod_cat': prod_cat})
+    
+
+def catalogue(request, pk=None):
+    print(pk)
+    prod_cat = ProductCategory.objects.all()
+    basket = get_basket(request.user)
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+    if pk is not None:
+        if pk == '0':
+            products = Product.objects.all().order_by('price')
+            prod_cat_all = {'name': 'Все'}
+        else:
+            prod_cat_all = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by('price')
+
+        return render(request, 'mainapp/catalogue/products_list.html', context={'user': request.user,
+                                                                        'menu': {'main': 'Главная',
+                                                                                 'catalogue': 'Каталог',
+                                                                                 'contacts': 'Контакты'},
+                                                                        'page': 'Каталог', 'products': products,
+                                                                        'prod_cat_all': prod_cat_all, 'prod_cat': prod_cat})
+
+
+    return render(request, 'mainapp/catalogue/index.html', context={'user': request.user,
+                                                                    'menu': {'main': 'Главная',
+                                                                             'catalogue': 'Каталог',
+                                                                             'contacts': 'Контакты'},
+                                                                    'page': 'Каталог',
+                                                                    'products': same_products, 'prod_cat': prod_cat, 'hot_product': hot_product, 'same_products': same_products})
+
+
+def contacts(request):
+    prod_cat = ProductCategory.objects.all()
+    return render(request, 'mainapp/contacts/index.html', context={'user': request.user, 'placeholder': {'name': 'ivan', 'email': 'mail@gmail.com', 'theme': 'покупка монитора'}, 'prod_cat': prod_cat, 'menu': {'main': 'Главная', 'catalogue': 'Каталог', 'contacts': 'Контакты'}, 'page': 'Контакты'})
+
+
+def monitor(request):
+    prod_cat = ProductCategory.objects.all()
+    return render(request, r'mainapp/catalogue/monitor/Eizo ColorEdge CG318-4K/CG318-4K.html', context={'user': request.user, 'menu': {'main': 'Главная', 'catalogue': 'Каталог', 'contacts': 'Контакты'}, 'prod_cat': prod_cat})
+# Create your views here.
+
+
+def product(request, pk):
+    title = 'Продукт'
+    prod_cat = ProductCategory.objects.all()
+    context = {'title': title,
+               'menu': {'main': 'Главная', 'catalogue': 'Каталог', 'contacts': 'Контакты'},
+               'product': get_object_or_404(Product, pk=pk),
+               'prod_cat': prod_cat
+               }
+    return render(request, 'mainapp/catalogue/product/product.html', context)
+
+# Create your views here.
